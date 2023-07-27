2c2082fa872d20715bfbc47184ba9333f8486054
RossiGiuliano <rossi_giuliano@hotmail.com>
2023-07-27 16:15:52 -0300

    agregamos prod.py

---
+++ blogProject/settings/prod.py
@@ -0,0 +1,16 @@
+from .base import *
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+
+DATABASES = {  
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'PASSWORD': 'Tulita2000',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
