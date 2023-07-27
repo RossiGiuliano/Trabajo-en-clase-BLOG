0281a476d0c54eed1fa1610685179ed80f9fd42c
RossiGiuliano <rossi_giuliano@hotmail.com>
2023-07-27 16:08:07 -0300

    Likes

--- publicaciones/models.py
+++ publicaciones/models.py
@@ -19,7 +19,7 @@ class Publicaciones(models.Model):
     post = models.TextField()
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, related_name='posteos', null= True)
     creador = models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='posteos_usario')
-
+    meGusta = models.ManyToManyField(Usuario, related_name='publicaciones_gustadas', blank=True)
 
     def __str__(self):
         return self.titulo
